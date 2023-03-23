12ef06e0927b1d6c92439ab5c3a92d679be64577
Marlon-Orrego <marlonorrego11@gmail.com>
2023-03-22 23:00:27 -0500

    Correciones Diseño Home

--- todov2/lib/app_ejemplo_1/main_app_ejemplo_1.dart
+++ todov2/lib/app_ejemplo_1/main_app_ejemplo_1.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:todov2/app_ejemplo_1/pages/home.dart';
 import 'package:todov2/app_ejemplo_1/pages/character.dart';
 import 'package:todov2/app_ejemplo_1/pages/list_characters.dart';
+import 'package:flutter/services.dart';
+
+Future<void> loadFont() async {
+  await rootBundle.load("assets/fonts/MarvelRegular-Dj83.ttf");
+}
 
 class MainAppEjemplo1 extends StatefulWidget {
   @override
@@ -36,6 +41,10 @@ class _MainAppEjemplo1 extends State<MainAppEjemplo1> {
           backgroundColor: Colors.black,
           currentIndex: currentlyIndex,
           selectedItemColor: Color.fromARGB(255, 255, 0, 0),
+          selectedLabelStyle:
+              TextStyle(fontFamily: 'MarvelRegular', fontSize: 20.0),
+          unselectedLabelStyle:
+              TextStyle(fontFamily: 'MarvelRegular', fontSize: 20.0),
           unselectedItemColor: Color.fromARGB(255, 255, 255, 255),
           items: const [
             BottomNavigationBarItem(
